c25cb54f2accdf9eadcbe968caa4af704c662262
sre-priscilla <943459049@qq.com>
2021-01-17 16:18:06 +0800

    53

--- offer_53_I.py
+++ offer_53_I.py
@@ -2,9 +2,27 @@ from typing import List
 
 
 class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        # i, j = 0, len(nums) - 1
-        # while i < j:
-        #     m = i + (j - i) // 2
-        pass
+    def search(self, nums: List[int], target: int) -> int:
+        i, j = 0, len(nums) - 1
+        while i <= j:
+            m = i + (j - i) // 2
+            if nums[m] <= target:
+                i = m + 1
+            else:
+                j = m - 1
+        right = i
+
+        if j >= 0 and nums[j] != target:
+            return 0
+
+        i = 0
+        while i <= j:
+            m = i + (j - i) // 2
+            if nums[m] < target:
+                i = m + 1
+            else:
+                j = m - 1
+        left = j
+
+        return right - left - 1
 
